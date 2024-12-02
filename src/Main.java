d0fdd6ba95283352c4dace383f8fd4a82d540df1
Laura Fumagalli Rodrigues <fulanodetal@exemplo.br>
2024-12-02 16:54:32 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int number = 20;
+        String phrase = "Hello world!";
+        System.out.println(phrase);
+        System.out.println(number);
+        System.out.println(number);
+        System.out.println(number);
+        System.out.println(number);
+        System.out.println(number);
+        System.out.println(number);
+    }
+}
